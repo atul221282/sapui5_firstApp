f70723352a60e106701591f6dccff2f6a0c1eac6
atul221282 <atul221282@gmail.com>
2018-03-30 01:00:55 +1030

    first commit

---
+++ WebContent/sapui_firstapp/firstApp.view.js
@@ -0,0 +1,24 @@
+sap.ui.jsview("sapui_firstapp.firstApp", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf sapui_firstapp.firstApp
+	*/ 
+	getControllerName : function() {
+		return "sapui_firstapp.firstApp";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf sapui_firstapp.firstApp
+	*/ 
+	createContent : function(oController) {
+ 		return new sap.m.Page({
+			title: "Title",
+			content: [
+			
+			]
+		});
+	}
+
+});
